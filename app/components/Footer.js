68721270d2d20817d45566ed27461b3648aeb78b
gaurav parsar <98256253+INDIANgaurav@users.noreply.github.com>
2024-03-04 16:40:23 +0530

    complete upto movie page

---
+++ app/components/Footer.js
@@ -0,0 +1,27 @@
+// import React from "react";
+// import github from "../assets/github.png";
+// import linkedin from "../assets/linkedin.png";
+
+// const Footer = () => {
+//   return (
+//     <footer className="flex flex-col md:flex-row items-center justify-center bg-neutral-200 text-center dark:bg-neutral-500 lg:text-left">
+//       <div className=" p-4 text-center text-neutral-700 dark:text-neutral-200">
+//         © 2024 Copyright:
+//         <a className="text-neutral-800 dark:text-neutral-400">
+//           {" "}
+//           ❤️ Glamour-By-Gaurav
+//         </a>
+//       </div>
+//       <div className="flex gap-2 w-12 ">
+//         <a href="https://github.com/INDIANgaurav" target="_blank">
+//           <img src={github} alt="githubimage" />
+//         </a>
+//         <a href="https://www.linkedin.com/in/gaurav-parasar" target="_blank">
+//           <img src={linkedin} alt="linkedinimage" />
+//         </a>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import HeroSection from "./components/HeroSection";
+
+ 
+
+export default function Home() {
+  return (
+     <>
+      <HeroSection title={"LET\'S WATCH MOVIE TOGETHER"} imageUrl={"/home.svg"} />
+    
+     </>
+  );
+}
